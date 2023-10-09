4f60b0096ec8c75c6daa4b3c5cb5c0c580e8a2d3
Rafael Bahiense <rafaelbahiense@id.uff.br>
2023-10-09 10:37:49 -0300

    feat: DBConnect

---
+++ FitSystem/src/fitsystem/DBConnect.java
@@ -0,0 +1,44 @@
+package fitsystem;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnect {
+
+    private String DB_URL;
+    private String USER;
+    private String PASS;
+    
+    private Connection conn;
+    
+    public DBConnect(String db_url, String user, String pass) throws SQLException {
+
+        DB_URL = db_url;
+        USER = user;
+        PASS = pass;
+
+        connect();
+    }
+
+    private void connect() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public Connection getConnection() {
+        return conn;
+    }
+    
+    public void close() throws SQLException {
+        if (conn != null) {
+            conn.close();
+        }
+    }
+}
